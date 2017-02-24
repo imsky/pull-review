eee8d9e6bf814f726ceef527d33c0a5a334a90f6
Ivan Malopinsky <imsky@users.noreply.github.com>
2017-02-23 21:17:38 -0500

    add slack, tweak gitignore

---
+++ src/slack.js
@@ -0,0 +1,52 @@
+var config = require('./config');
+var url = require('./url');
+
+var extractURLs = url.extractURLs;
+
+var GITHUB_ICON_URL = config.GITHUB_ICON_URL;
+
+function generateSlackAttachmentFromGithubResource (resource) {
+  var data = resource.data;
+
+  if (!data) {
+    throw Error('Missing resource data');
+  }
+
+  var repoName = resource.owner + '/' + resource.repo;
+  var body = data.body;
+  var imagesInBody = false;
+  var keyImage;
+
+  var bodyURLs = extractURLs(body);
+
+  for (var i = 0; i < bodyURLs.length; i++) {
+    if (bodyURLs[i].match(/\.(jpg|jpeg|png|gif)$/)) {
+      imagesInBody = true;
+      keyImage = bodyURLs[i];
+      break;
+    }
+  }
+
+  var attachment = {
+    'fallback': data.title + ' by ' + data.user.login + ': ' + data.html_url,
+    'author_name': data.user.login,
+    'author_link': data.user.html_url,
+    'title': repoName + ': ' + data.title,
+    'title_link': data.html_url,
+    'text': imagesInBody ? '' : body,
+    'mrkdwn_in': ['text', 'pretext', 'fields'],
+    'color': '#24292e',
+    'footer': 'GitHub',
+    'footer_icon': GITHUB_ICON_URL
+  };
+
+  if (imagesInBody) {
+    attachment.image_url = keyImage;
+  }
+
+  return attachment;
+}
+
+module.exports = {
+  'generateSlackAttachmentFromGithubResource': generateSlackAttachmentFromGithubResource
+};
