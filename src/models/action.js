a384d0a77741ce6339328bd3aabddb22bacb03c7
Ivan Malopinsky <imsky@users.noreply.github.com>
2017-09-09 19:09:42 -0400

    add action validation

--- src/models/action.js
+++ src/models/action.js
@@ -1,9 +1,13 @@
+var SUPPORTED_ACTIONS = ['COMMIT', 'NOTIFY', 'ASSIGN_USERS_TO_PULL_REQUEST', 'UNASSIGN_USERS_FROM_PULL_REQUEST'];
+
 module.exports = function Action(input) {
   var type = input.type;
   var payload = input.payload;
 
   if (!type || !payload) {
     throw Error('Missing action data');
+  } else if (SUPPORTED_ACTIONS.indexOf(type) === -1) {
+    throw Error('Unsupported action: ' + type);
   }
 
   return input;
